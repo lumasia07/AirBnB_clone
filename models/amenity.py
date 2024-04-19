e8d4ff72eb1b043704cc17fe5642d6a335adfc37
lumasia07 <lumasiastancelaus@gmail.com>
2024-04-19 13:18:47 -0700

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 """Defines a class Amenity"""
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalhemy.orm import relationship
+from os import getenv
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Amenity of BnB
 
     Args:
         name(str): name of amenity
     """
-    name = ""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
